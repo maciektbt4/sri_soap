96b52d922f967ced686b860f3cd6e67b5542f013
maciektbt4 <mbkrzak@gmail.com>
2023-05-24 23:15:42 +0200

    015 SOAP: crud functionality is done

--- rpctest/core/serializers.py
+++ rpctest/core/serializers.py
@@ -6,5 +6,5 @@ class PersonSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Person
-		fields = ['id', 'sex', 'first_name', 'last_name', 'job', 'email', 'birth_date']
+		fields = ['id', 'sex', 'hight', 'first_name', 'last_name', 'job', 'email', 'birth_date']
 
--- rpctest/core/views.py
+++ rpctest/core/views.py
@@ -1,58 +1,23 @@
-#!/usr/bin/env python
-# encoding: utf8
-#
-# Copyright © BJ Cardon <bj dot car dot don at gmail dot com>,
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#    1. Redistributions of source code must retain the above copyright notice,
-#       this list of conditions and the following disclaimer.
-#    2. Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#    3. Neither the name of the owner nor the names of its contributors may be
-#       used to endorse or promote products derived from this software without
-#       specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT,
-# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-
-from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 from django.views.decorators.csrf import csrf_exempt
-
 from spyne.error import ResourceNotFoundError, ResourceAlreadyExistsError
 from spyne.server.django import DjangoApplication
 from spyne.model.primitive import Unicode, Integer
 from spyne.model.complex import Iterable
+from spyne.model.complex import Array
 from spyne.service import Service
 from spyne.protocol.soap import Soap11
 from spyne.application import Application
 from spyne.decorator import rpc
 from spyne.util.django import DjangoComplexModel, DjangoService
-
-from .serializers import PersonSerializer
 from .models import Person
-
-# from rpctest.core.models import FieldContainer
+from .serializers import PersonSerializer
 
 
 class PersonContainer(DjangoComplexModel):
     class Attributes(DjangoComplexModel.Attributes):
         django_model = Person
         #django_exclude = ['excluded_field']
-
 
 class HelloWorldService(Service):
     @rpc(Unicode, Integer, _returns=Iterable(Unicode))
@@ -65,8 +30,16 @@ class PersonService(Service):
     @rpc(Integer, _returns=PersonContainer)
     def get_person(ctx, pk):
         try:
-            print(f"-------------Hello persons------------{pk}")
             return Person.objects.get(pk=pk)
+        except Person.DoesNotExist:
+            raise ResourceNotFoundError('PersonContainer')
+    
+    @rpc(_returns=Array(PersonContainer))
+    def get_all_persons(ctx):
+        try:
+            persons = Person.objects.all()            
+            return persons
+            
         except Person.DoesNotExist:
             raise ResourceNotFoundError('PersonContainer')
 
@@ -76,7 +49,29 @@ class PersonService(Service):
             return Person.objects.create(**container.as_dict())
         except IntegrityError:
             raise ResourceAlreadyExistsError('PersonContainer')
+    
+    @rpc(Integer,_returns=Unicode)
+    def delete_person(ctx, pk):
+        try:
+            person_to_delete = Person.objects.filter(pk=pk)
+            person_to_delete.delete()
+            
+            return f"Person id = {pk} has been deleted"
+        except IntegrityError:
+            raise ResourceAlreadyExistsError('PersonContainer')
 
+    @rpc(Integer,PersonContainer, _returns=PersonContainer)
+    def update_person(ctx, pk,  container):
+        try:
+            person = Person.objects.get(pk = pk)  
+            data = container.as_dict()
+            serializer=PersonSerializer(person, data=data)
+            serializer.is_valid()
+            serializer.save()
+            return person
+        except IntegrityError:
+            raise ResourceAlreadyExistsError('PersonContainer')
+        
 class ExceptionHandlingService(DjangoService):
 
     """Service for testing exception handling."""
@@ -84,10 +79,6 @@ class ExceptionHandlingService(DjangoService):
     @rpc(_returns=PersonContainer)
     def raise_does_not_exist(ctx):
         return Person.objects.get(pk=-1)
-
-    # @rpc(_returns=PersonContainer)
-    # def raise_validation_error(ctx):
-    #     raise ValidationError(None, 'Invalid.')
 
 
 app = Application([HelloWorldService, PersonService,
